c9f15536ec9108bb59277d04637caa3bcd9e2b12
Patrick Nguyen <nqhuy25@cmcglobal.vn>
2024-11-10 14:44:48 +0700

    Update VSCode settings and enhance Tailwind CSS configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
+  // Content paths for PurgeCSS to scan
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -8,17 +7,26 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}',
     './node_modules/flowbite/**/*.js'
   ],
+
+  // Enable dark mode via class strategy
+  darkMode: 'class',
+
+  // Theme customizations
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
+
+  // Plugins to enhance Tailwind CSS
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/container-queries'),
-    require('flowbite/plugin'),
-  ]
+    require('flowbite/plugin')({
+      charts: true,
+      forms: true,
+      tooltips: true
+    }),
+    // ...additional plugins...
+  ],
 }
