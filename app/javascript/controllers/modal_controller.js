d539f30e13fb06bc3fbd08d62241dd1bd2162b96
Patrick Nguyen <nqhuy25@cmcglobal.vn>
2024-11-13 17:33:37 +0700

    Enhance UserContact form with Turbo support and improve modal handling

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -8,16 +8,23 @@ export default class extends Controller {
   }
 
   open() {
+    console.log("open")
     this.overlayTarget.classList.remove("hidden")
     this.modalTarget.classList.remove("hidden")
   }
 
   close() {
+    console.log("close")
     this.overlayTarget.classList.add("hidden")
     this.modalTarget.classList.add("hidden")
   }
 
   handleSuccess(event) {
-    this.close()
+    if (event.detail.success) {
+      console.log("Form submitted successfully")
+      this.close()
+    } else {
+      console.log("Form submission failed")
+    }
   }
 }
