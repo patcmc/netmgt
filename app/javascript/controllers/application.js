1a7e27ecc6c808ec8ef1409e512451bd361739a0
Patrick Nguyen <nqhuy25@cmcglobal.vn>
2024-11-14 13:34:12 +0700

    Refactor components to standardize naming conventions and improve Turbo functionality

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import ContactModalController from "./contacts/contact_modal_controller"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-application.register("contactModal", ContactModalController)
 
 export { application }
