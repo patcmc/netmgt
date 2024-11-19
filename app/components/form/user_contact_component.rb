5a77f2febded0e904dd3e217268f21b8b8f75ac6
Patrick Nguyen <nqhuy25@cmcglobal.vn>
2024-11-19 13:45:30 +0700

    Add user contact details component and refactor user contact card for improved structure and readability

--- app/components/form/user_contact_component.rb
+++ app/components/form/user_contact_component.rb
@@ -7,5 +7,13 @@ module Form
       @user_contact = user_contact || UserContact.new
       @url = url
     end
+
+    def cancel_path
+      @user_contact.new_record? ? app_contacts_path : app_contact_path(@user_contact)
+    end
+
+    def submit_label
+      @user_contact.new_record? ? "Create" : "Save"
+    end
   end
 end
