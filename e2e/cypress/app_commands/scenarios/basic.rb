c78b808ba6c53c3cee09298625ef9fa9c2c34a66
Patrick Nguyen <nqhuy25@cmcglobal.vn>
2024-11-23 15:49:04 +0700

    Add Cypress integration with support files, commands, and user login tests

---
+++ e2e/cypress/app_commands/scenarios/basic.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+FactoryBot.create(:user, email: "user@example.com", password: "password")
---
+++ e2e/cypress/e2e/auth/login_spec.cy.js
@@ -0,0 +1,24 @@
+describe('User Login', () => {
+  beforeEach(() => {
+    cy.visit('/users/sign_in')
+  })
+
+  // it('allows a user to log in with valid credentials', () => {
+  //   // Fill in the login form
+  //   cy.get('input[name="user[email]"]').type('user@example.com')
+  //   cy.get('input[name="user[password]"]').type('password')
+  //   cy.get('form').submit()
+
+  //   // Assert that we were redirected to the dashboard URL
+  //   cy.url().should('eq', `${Cypress.config().baseUrl}/app`)
+  // })
+
+  it('shows an error with invalid credentials', () => {
+    cy.get('input[name="user[email]"]').type('invalid@example.com')
+    cy.get('input[name="user[password]"]').type('wrongpassword')
+    cy.get('form').submit()
+    
+    // Optionally, ensure the URL hasn't changed
+    cy.url().should('eq', `${Cypress.config().baseUrl}/users/sign_in`)
+  })
+})
